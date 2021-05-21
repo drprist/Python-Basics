d37b4416ef71b314fb48c52c8a896516991fc5d3
Сергей <glanev92@gmail.com>
2021-05-21 19:38:08 +0300

    Lesson_1

---
+++ task1.py
@@ -0,0 +1,9 @@
+num_1 = 1
+num_2 = 2
+num_3 = num_1 + num_2
+print(num_1, num_2, num_3)
+
+name = input("Введите имя: ")
+surname = input("Введите фамилию: ")
+age = input("Введите возраст: ")
+print("Ваши данные: ", name, surname, age, "лет")
